51475dc817b2c05f6a346bd4bbafb476006e3d1f
Konstantinos Kastritis <dinokastritis@gmail.com>
2017-06-27 11:07:18 -0400

    bug fixes

--- polymer-normal.tcl
+++ polymer-normal.tcl
@@ -51,10 +51,10 @@ inter 1 0 lennard-jones $eps $sigma $lj_cutoff $lj_shift $lj_offset
 set t_trans 0
 set trans_flag 0
 set fixed_N [expr $N/2]
-set equil_time [expr 10.0 * $N]
+set equil_time [expr 100.0 * $N]
 set t_pore 1
 set z_line [expr $cz - $t_pore/2]
-set force [expr -1.13]
+set force [expr -4.3]
 set lsens 1.5
 set lcav 19.5 
 set rcav 99.5
@@ -77,9 +77,9 @@ part [expr $N+4] pos $cx $cy [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfi
 
 constraint pore center [expr $cx] [expr $cy] [expr $cz] axis 0 0 1 radius $rsens length $rsens type 1
 constraint pore center [expr $cx] [expr $cy] [expr $cz + $lcav+0.5 + $lsens+0.5] axis 0 0 1 radius $rcav length $lcav type 1
-# constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5] axis 0 0 1 radius $rfilt length $lfilt type 1
-# constraint pore center [expr $cx] [expr $cy] [expr $cz + $lcav+0.5 + $lsens+0.5 + $mporezfilt] axis 0 0 1 radius $rcav length $lcav type 1
-# constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5 + $mporezfilt] axis 0 0 1 radius $rsens length $lsens type 1
+constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5] axis 0 0 1 radius $rfilt length $lfilt type 1
+#constraint pore center [expr $cx] [expr $cy] [expr $cz + $lcav+0.5 + $lsens+0.5 + $mporezfilt] axis 0 0 1 radius $rcav length $lcav type 1
+#constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5 + $mporezfilt] axis 0 0 1 radius $rsens length $lsens type 1
 
 
 
@@ -112,7 +112,7 @@ if { $vis == 1 } {
 
 
 
-part 0 fix
+
 
 set flag 0
 set t 0	
@@ -122,56 +122,62 @@ set success 0
 set trans_flag 0
 
 
-
+set position_flag 0
 while {$flag == 0} {
 	#puts "debug while"
-	for { set i 0 } { $i < $N } { incr i } {
-		# set x [expr $cx]
-		# set y [expr $cy]
-		# set z [expr $cz  + 0.97*$i]
+	part 0 fix
+	if {$position_flag == 1} {
+		puts "here"
+		for { set i 0 } { $i < $N } { incr i } {
+			set x [expr $cx]
+			set y [expr $cy]
+			set z [expr $cz  + 0.97*$i]
 
-	# 	set x [expr $cx - $N/2 + $i]
-	# 	set y [expr $cy]
-	# 	set z [expr $cz + 15]
-	# 	part $i pos $x $y $z type 0 
+			# set x [expr $cx - $N/2 + $i]
+			# set y [expr $cy]
+			# set z [expr $cz + 15]
+			part $i pos $x $y $z ext_force 0 0 0
+		}
+		set position_flag 0
 	}
 	# integrate 100
 	# imd positions
 	#part $fixed_N fix
-
-	thermostat langevin $temp $gamma_equilibration
-	for {set i 0} {$i < $equil_time} {incr i} {
-	    set z_list {}
-		for {set i 0} { $i < $N } {incr i} {
-			set x [lindex [part $i print pos] 0]
-			set y [lindex [part $i print pos] 1]
-			set z [lindex [part $i print pos] 2]
-			set r [expr sqrt(($x-$cx)*($x-$cx) + ($y-$cy)*($y-$cy) + ($z-$cz)*($z-$cz))]
-			#puts $r
-			lappend z_list $z
-			lappend r_list $r
-			#puts "hi, I'm getting particle positions"
-		}
-		set z_min [::tcl::mathfunc::min {*}$z_list]
-		set z_max [::tcl::mathfunc::max {*}$z_list]
+	
 
 
-		if {$z_max < [lindex [part [expr $N+4] print pos] 2] } {
-			break
-		}
+	# thermostat langevin $temp $gamma_equilibration
+	# for {set i 0} {$i < $equil_time} {incr i} {
+	#     set z_list {}
+	# 	for {set i 0} { $i < $N } {incr i} {
+	# 		set x [lindex [part $i print pos] 0]
+	# 		set y [lindex [part $i print pos] 1]
+	# 		set z [lindex [part $i print pos] 2]
+	# 		set r [expr sqrt(($x-$cx)*($x-$cx) + ($y-$cy)*($y-$cy) + ($z-$cz)*($z-$cz))]
+	# 		#puts $r
+	# 		lappend z_list $z
+	# 		lappend r_list $r
+	# 		#puts "hi, I'm getting particle positions"
+	# 	}
+	# 	set z_min [::tcl::mathfunc::min {*}$z_list]
+	# 	set z_max [::tcl::mathfunc::max {*}$z_list]
 
-	    integrate 100
-	    imd positions	
-	}
-	thermostat langevin $temp $gamma
 
-	part 0 fix
+	# 	if {$z_max < [lindex [part [expr $N+4] print pos] 2] } {
+	# 		break
+	# 	}
+
+	#     integrate 100
+	#     imd positions	
+	# }
+	# thermostat langevin $temp $gamma
+
 
 	#part $fixed_N unfix
 
 	puts "equilibrated."
 
-	part [expr $N-1] fix
+	#part [expr $N-1] fix
 	puts "fixed"
 	thermostat langevin $temp $gamma_equilibration
 
@@ -182,7 +188,7 @@ while {$flag == 0} {
 	}
 	thermostat langevin $temp $gamma
 
-	part [expr $N-1] unfix
+	#part [expr $N-1] unfix
 	puts "unfixed"
 
 	#puts "[lindex [part [expr $N+4] print pos] 2]"
@@ -219,11 +225,7 @@ while {$flag == 0} {
 		set z_max [::tcl::mathfunc::max {*}$z_list]
 
 
-		if {$z_min > [lindex [part [expr $N+4] print pos] 2] } {
-			puts "here"
-			break
-		}
-		if {$z_min < $z_line} {
+		if {($z_min < $z_line)} {
 			
 			if {$trans_flag == 0 } {
 				#puts "inside translocation if"
@@ -235,9 +237,23 @@ while {$flag == 0} {
 				set trans_flag [expr $trans_flag + 1 ]
 			}
 		}
-		#puts $z_max
-		if {$z_max < $z_line} {
-			puts "zmax less than zline"
+
+
+		if {$z_min > $z_top} {
+			
+			if {$trans_flag == 0 } {
+				#puts "inside translocation if"
+				set t_thread $t
+				puts "$t_thread"
+
+				set rg_calc_trans [analyze rg 0 1 $N]
+
+				set trans_flag [expr $trans_flag + 1 ]
+			}
+		}
+
+		if {($z_min > [lindex [part [expr $N+4] print pos] 2]) || ($z_max < $z_line)} {
+			puts "translocating"
 			set t_last_thread $t
 			puts $t_last_thread
 			set t_trans [expr $t_last_thread - $t_thread]
@@ -246,11 +262,27 @@ while {$flag == 0} {
 			close $trans_time
 			set n [expr $n + 1.0]
 			set trans_flag 0
+			set position_flag 1
 			break
-
 		}
+
+		#puts $z_max
+		# if {$z_max < $z_line} {
+		# 	puts "zmax less than zline"
+		# 	set t_last_thread $t
+		# 	puts $t_last_thread
+		# 	set t_trans [expr $t_last_thread - $t_thread]
+		# 	set trans_time [open "data/${filename}_$N/trans_time_$N-$rseed.dat" "a"]
+		# 	puts $trans_time "$t_trans $N $t_last_thread $t_thread"
+		# 	close $trans_time
+		# 	set n [expr $n + 1.0]
+		# 	set trans_flag 0
+		# 	break
+
+		# }
 		if {$t_trans != 0} {
 			set t_trans 0 
+			
 		}
 		integrate 100
 		imd positions
