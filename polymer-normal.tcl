bdae03dd22fcd8fcd13c9bbce128b7ec6c529c64
Konstantinos Kastritis <dinokastritis@gmail.com>
2017-07-05 14:29:10 -0400

    added metrics v1

--- polymer-normal.tcl
+++ polymer-normal.tcl
@@ -12,9 +12,9 @@ set boxx 500
 set boxy 500
 set boxz 500 
 
-set cx [expr $boxx/10.0]
-set cy [expr $boxy/10.0]
-set cz [expr $boxz/10.0]
+set cx [expr $boxx/2.0]
+set cy [expr $boxy/2.0]
+set cz [expr $boxz/2.0]
 set tmax 10000
 set nmax 10
 set temp 1.0 ; set gamma 1.0; set gamma_equilibration 0.1
@@ -47,11 +47,10 @@ inter 0 fene $k_fene $r_fene
 inter 1 angle $k_angle $pi
 inter 0 0 lennard-jones $eps $sigma $lj_cutoff $lj_shift $lj_offset
 inter 1 0 lennard-jones $eps $sigma $lj_cutoff $lj_shift $lj_offset
-
 set t_trans 0
 set trans_flag 0
 set fixed_N [expr $N/2]
-set equil_time [expr 10]
+set equil_time [expr $N * $N]
 set t_pore 1
 set z_line [expr $cz - $t_pore/2]
 set force [expr -4.3]
@@ -76,24 +75,24 @@ part [expr $N+3] pos $cx $cy [expr $cz + $lcav+0.5 + $lsens+0.5 + $mporezfilt] t
 part [expr $N+4] pos $cx $cy [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5 + $mporezfilt] type 99 fix
 
 constraint pore center [expr $cx] [expr $cy] [expr $cz] axis 0 0 1 radius $rsens length $rsens type 1
-constraint pore center [expr $cx] [expr $cy] [expr $cz + $lcav+0.5 + $lsens+0.5] axis 0 0 1 radius $rcav length $lcav type 1
-constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5] axis 0 0 1 radius $rfilt length $lfilt type 1
+#constraint pore center [expr $cx] [expr $cy] [expr $cz + $lcav+0.5 + $lsens+0.5] axis 0 0 1 radius $rcav length $lcav type 1
+#constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5] axis 0 0 1 radius $rfilt length $lfilt type 1
 #constraint pore center [expr $cx] [expr $cy] [expr $cz + $lcav+0.5 + $lsens+0.5 + $mporezfilt] axis 0 0 1 radius $rcav length $lcav type 1
 #constraint pore center [expr $cx] [expr $cy] [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5 + $mporezfilt] axis 0 0 1 radius $rsens length $lsens type 1
 
 
 
-
+#puts [expr $cz + 2.0 * ($lcav + 0.5) + $lsens+0.5 + $lfilt+0.5 + $mporezfilt]
 
 for { set i 0 } { $i < $N } { incr i } {
 	#set x [expr $cx - $N/2 + $i]
 	set x [expr $cx]
 	set y [expr $cy]
-	set z [expr $cz  + 0.97*$i - 3]
+	set z [expr $cz  + 0.97*$i]
 	# set x [expr $cx - $N/2 + $i]
 	# set y [expr $cy]
-	# set z [expr $cz + 15]	
-	part $i pos $x $y $z type 0 fix 1 1 0
+	# set z [expr $cz + 85]	
+	part $i pos $x $y $z type 0
 
 
 	if { $i > 0 } {
@@ -131,54 +130,20 @@ while {$flag == 0} {
 		for { set i 0 } { $i < $N } { incr i } {
 			set x [expr $cx]
 			set y [expr $cy]
-			set z [expr $cz  + 0.97*$i - 3]
+			set z [expr $cz  + 0.97*$i]
 
 			# set x [expr $cx - $N/2 + $i]
 			# set y [expr $cy]
-			# set z [expr $cz + 15]
-			part $i pos $x $y $z ext_force 0 0 0 fix 1 1 0
+			# set z [expr $cz + 85]
+			part $i pos $x $y $z ext_force 0 0 0
 		}
 		set position_flag 0
 	}
-	# integrate 100
-	# imd positions
-	#part $fixed_N fix
-	
 
+	# puts "equilibrated."
 
-	# thermostat langevin $temp $gamma_equilibration
-	# for {set i 0} {$i < $equil_time} {incr i} {
-	#     set z_list {}
-	# 	for {set i 0} { $i < $N } {incr i} {
-	# 		set x [lindex [part $i print pos] 0]
-	# 		set y [lindex [part $i print pos] 1]
-	# 		set z [lindex [part $i print pos] 2]
-	# 		set r [expr sqrt(($x-$cx)*($x-$cx) + ($y-$cy)*($y-$cy) + ($z-$cz)*($z-$cz))]
-	# 		#puts $r
-	# 		lappend z_list $z
-	# 		lappend r_list $r
-	# 		#puts "hi, I'm getting particle positions"
-	# 	}
-	# 	set z_min [::tcl::mathfunc::min {*}$z_list]
-	# 	set z_max [::tcl::mathfunc::max {*}$z_list]
-
-
-	# 	if {$z_max < [lindex [part [expr $N+4] print pos] 2] } {
-	# 		break
-	# 	}
-
-	#     integrate 100
-	#     imd positions	
-	# }
-	# thermostat langevin $temp $gamma
-
-
-	#part $fixed_N unfix
-
-	puts "equilibrated."
-
-	#part [expr $N-1] fix
-	puts "fixed"
+	# #part [expr $N-1] fix
+	# puts "fixed"
 	thermostat langevin $temp $gamma_equilibration
 
 	for {set i 0} {$i < $equil_time} {incr i} {
@@ -188,15 +153,15 @@ while {$flag == 0} {
 	}
 	thermostat langevin $temp $gamma
 
-	#part [expr $N-1] unfix
-	puts "unfixed"
+	# #part [expr $N-1] unfix
+	# puts "unfixed"
 
-	#puts "[lindex [part [expr $N+4] print pos] 2]"
+	# #puts "[lindex [part [expr $N+4] print pos] 2]"
 	part 0 unfix
 
-	
 	#puts "I'm back in the first while
-
+	set monomers [open "data/${filename}_$N/monomers_$N-$rseed.dat" "a"]
+	
 
 	while {1} {
 		
@@ -211,6 +176,10 @@ while {$flag == 0} {
 			break
 		}
 
+		set ntrans 0
+		set mtrans 0
+
+
 		for {set i 0} { $i < $N } {incr i} {
 			set x [lindex [part $i print pos] 0]
 			set y [lindex [part $i print pos] 1]
@@ -219,13 +188,18 @@ while {$flag == 0} {
 			#puts $r0
 			lappend z_list $z
 			lappend r_list $r
+			if {$z < $z_line} {
+				set ntrans [expr $ntrans + 1] 
+			} elseif { $z > $z_top} {
+				set mtrans [expr $mtrans + 1]
+			}
 			#puts "hi, I'm getting particle positions"
 		}
 		set z_min [::tcl::mathfunc::min {*}$z_list]
 		set z_max [::tcl::mathfunc::max {*}$z_list]
 
 
-		if {$z_min > ([expr $cz + 2])} {
+		if {($z_min > ([expr $cz + 2]))} {
 			puts "retraction"
 			set position_flag 1
 			set trans_flag 0
@@ -240,26 +214,15 @@ while {$flag == 0} {
 				puts "$t_thread"
 
 				set rg_calc_trans [analyze rg 0 1 $N]
+				set re_calc_trans [analyze re 0 1 $N]
+
 
 				set trans_flag [expr $trans_flag + 1 ]
 			}
 		}
 
 
-		if {$z_min > $z_top} {
-			
-			if {$trans_flag == 0 } {
-				#puts "inside translocation if"
-				set t_thread $t
-				puts "$t_thread"
-
-				set rg_calc_trans [analyze rg 0 1 $N]
-
-				set trans_flag [expr $trans_flag + 1 ]
-			}
-		}
-
-		if {($z_min > [lindex [part [expr $N+4] print pos] 2]) || ($z_max < $z_line)} {
+		if {($z_max < $z_line)} {
 			puts "translocating"
 			set t_last_thread $t
 			puts $t_last_thread
@@ -267,26 +230,18 @@ while {$flag == 0} {
 			set trans_time [open "data/${filename}_$N/trans_time_$N-$rseed.dat" "a"]
 			puts $trans_time "$t_trans $N $t_last_thread $t_thread"
 			close $trans_time
+			set rg_at_trans [open "data/${filename}_$N/rg_at_trans_$N-$rseed.dat" "a"]
+			puts $rg_at_trans "$rg_calc_trans"
+			close $rg_at_trans
+			set re_at_trans [open "data/${filename}_$N/re_at_trans_$N-$rseed.dat" "a"]
+			puts $re_at_trans "$re_calc_trans"
+			close $re_at_trans
 			set n [expr $n + 1.0]
 			set trans_flag 0
 			set position_flag 1
 			break
 		}
 
-		#puts $z_max
-		# if {$z_max < $z_line} {
-		# 	puts "zmax less than zline"
-		# 	set t_last_thread $t
-		# 	puts $t_last_thread
-		# 	set t_trans [expr $t_last_thread - $t_thread]
-		# 	set trans_time [open "data/${filename}_$N/trans_time_$N-$rseed.dat" "a"]
-		# 	puts $trans_time "$t_trans $N $t_last_thread $t_thread"
-		# 	close $trans_time
-		# 	set n [expr $n + 1.0]
-		# 	set trans_flag 0
-		# 	break
-
-		# }
 		if {$t_trans != 0} {
 			set t_trans 0 
 			
@@ -296,5 +251,6 @@ while {$flag == 0} {
 		
 		incr t
 	}
+	close $monomers
 }
 
